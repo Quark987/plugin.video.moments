c2b02a8f08456a99f798cd16cc7a5c9f9b353bf0
Bert De Deckere <bert.dedeckere@icloud.com>
2021-01-23 15:51:11 +0100

    Cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Module: default
-# Author: Roman V. M.
-# Created on: 28.11.2014
+# Author: Bert De Deckere
+# Created on: 10.2020
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import sys
